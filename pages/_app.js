f63a94d677403c33a1c74e61149d4eb75b3a8428
giorgimshvid <Caspernumlock@gmail.com>
2021-08-24 01:33:59 +0300

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import React  from 'react';
+import React, { useEffect } from 'react';
+
+//next js
+import { useRouter } from 'next/router';
+
+//google analytic
+import * as gtag from '../google-analytics/gtag';
 
 //styles
 import '../styles/globals.scss';
@@ -11,6 +17,18 @@ import '../styles/blocks/_style-burger.scss';
 import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = url => {
+			gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<AnimatePresence exitBeforeEnter>
 			<Component {...pageProps} />;
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+
+//components
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+//google analytic
+import { GA_TRACKING_ID } from '../google-analytics/gtag';
 
 class MyDocument extends Document {
 	render() {
@@ -9,6 +14,23 @@ class MyDocument extends Document {
 					<link
 						href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&display=swap'
 						rel='stylesheet'
+					/>
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
 					/>
 				</Head>
 				<body>
